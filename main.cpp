aa0f1a442f12883fa8f2857d6b32a313173069c2
SuperMin0707 <62458794+SuperMin0707@users.noreply.github.com>
2020-12-26 15:43:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 int main()
-{	
+{	//VS Code配置C++环境
+	//1. 下载 VS Code
+	//2. 下载 MinGW,配置环境变量
+	//下载的文件：进入网站后不要点击 "Download Lasted Version"，往下滑，找到最新版的 "x86_64-posix-seh"。
+	//3. 下载插件Code Runner
+	//打开设置，搜索"run in terminal"，勾选该选项（否则程序无法在运行时使用键盘输入数据）
+	//OK
 	cout << "hello World!!!" <<endl;
 	return 0;
 }
